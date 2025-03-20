4ab7e1f56e95a773cfb61a06ce13f90c069fbf9b
James Baker <james@webpage.com>
2025-03-20 16:51:48 -0400

    added post request for predict

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
 const collectionName = process.env.MONGO_DB_COLLECTION;
-const mgmtcollectionName= process.env.MGMT_MONGO_DB_COLLECTION;
+const mgmtcollectionName = process.env.MGMT_MONGO_DB_COLLECTION;
 
 // Server Port Config -- @ localhost 3000
 const app = express();
@@ -67,6 +67,24 @@ app.get('/mgmt', async (req, res) => {
     }
 });
 
+app.post('/predict', async (req, res) => {
+    try {
+        const {job_role, office_loc} = req.body;
+        const response = await fetch('http://localhost:5000/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(job_role, office_loc)
+        });
+        const responseData = await response.json();
+        res.json(responseData);
+    } catch (error) {
+        res.status(500).json({ message: 'Error sending data', error: error.message });
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Hello world! \nServer is running on http://localhost:${PORT}`);
 });
@@ -84,17 +102,17 @@ app.post('/login', async (req, res) => {
 
             let nameToFind = null;
 
-            if (username === "cZebedee") nameToFind = {"firstName": "Christel", "lastName": "Zebedee"} // EMPLOYEE
-            else if (username === "lCharter") nameToFind = {"firstName": "Luke", "lastName": "Charter"} // HR
-            else if (username === "gLevet") nameToFind = {"firstName": "Godwin", "lastName": "Levet"} // MGMT
+            if (username === "cZebedee") nameToFind = { "firstName": "Christel", "lastName": "Zebedee" } // EMPLOYEE
+            else if (username === "lCharter") nameToFind = { "firstName": "Luke", "lastName": "Charter" } // HR
+            else if (username === "gLevet") nameToFind = { "firstName": "Godwin", "lastName": "Levet" } // MGMT
             console.log(`Looking for ${nameToFind.firstName + nameToFind.lastName}`)
 
             try {
                 const client = await MongoClient.connect(url);
                 const db = client.db(dbName);
                 const collection = db.collection(collectionName);
-                const user = await collection.findOne({first_name: nameToFind.firstName, last_name: nameToFind.lastName});
-                
+                const user = await collection.findOne({ first_name: nameToFind.firstName, last_name: nameToFind.lastName });
+
                 if (user.job_role.includes("HR")) {
                     user.job_role = "HR"
                 } else if (user.job_role.includes("Manager")) {
@@ -104,13 +122,13 @@ app.post('/login', async (req, res) => {
                 console.log(`ROLE ASSIGNED ==> ${user.job_role}`)
                 console.log(`Who is their manager? ${user.manager_id}`)
 
-                res.status(200).json({ success: true, message: 'Login successful!', userID: user._id, userEID: user.id, name: user.first_name + " " + user.last_name, role: user.job_role, managerID: user.manager_id});
+                res.status(200).json({ success: true, message: 'Login successful!', userID: user._id, userEID: user.id, name: user.first_name + " " + user.last_name, role: user.job_role, managerID: user.manager_id });
             } catch (err) {
                 console.error("Error:", err);
                 res.status(500).send("ERROR: Unable to find user from server");
             }
 
-            
+
         } else {
             res.status(401).json({ success: false, message: 'Invalid username or password' });
         }
